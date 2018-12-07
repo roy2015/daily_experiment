f63fcca8be5f49614471a3ae7b5df7d9da165d65
roy2015a <roy2011@163.com>
2018-12-07 10:27:02 +0800

    初始提交20181207

---
+++ daily_experiment_core/src/test/java/com/roy/maintest/MainTest.java
@@ -0,0 +1,16 @@
+package com.roy.maintest;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by apple on 2018/12/7.
+ */
+public class MainTest {
+
+    @Test
+    public void test1() {
+
+    }
+
+
+}
