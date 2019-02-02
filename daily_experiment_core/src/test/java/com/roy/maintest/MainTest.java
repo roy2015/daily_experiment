44ab6a02e8d4bd7ec00dd7ad98acb4636639c5f4
roy2015a <roy2011@163.com>
2019-02-02 10:49:52 +0800

    sessionId生成类的测试

--- daily_experiment_core/src/test/java/com/roy/maintest/MainTest.java
+++ daily_experiment_core/src/test/java/com/roy/maintest/MainTest.java
@@ -1,6 +1,9 @@
 package com.roy.maintest;
 
+import com.roy.miscellaneous.TestBloomFilter;
 import com.roy.miscellaneous.TestHashFunc;
+import com.roy.miscellaneous.TestSessionIdGenarate;
+import com.roy.miscellaneous.targetObject.SessionIdUtil;
 import org.testng.annotations.Test;
 
 /**
@@ -10,8 +13,19 @@ public class MainTest {
 
     @Test
     public void test1() {
-//        new TestBloomFilter().bloomFilter();
+        new TestBloomFilter().bloomFilter();
+    }
+
+    @Test
+    public void test2() {
         new TestHashFunc().test1();
+    }
+
+    @Test
+    public void testSessionIdGenerate() {
+        for (int i = 0; i < 5; i++) {
+            new TestSessionIdGenarate().test();
+        }
     }
 
 
