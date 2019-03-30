24bb3e80b85b08826d5918e5e8c28bafe80fc921
roy2015a <roy2011@163.com>
2019-03-30 13:06:36 +0800

    合并算法提交201903030

--- daily_experiment_core/src/test/java/com/roy/maintest/MainTest.java
+++ daily_experiment_core/src/test/java/com/roy/maintest/MainTest.java
@@ -3,8 +3,12 @@ package com.roy.maintest;
 import com.roy.miscellaneous.TestBloomFilter;
 import com.roy.miscellaneous.TestHashFunc;
 import com.roy.miscellaneous.TestSessionIdGenarate;
+import com.roy.miscellaneous.arithmetic.TestMergeSort;
 import com.roy.miscellaneous.targetObject.SessionIdUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.Test;
+
+import java.util.Random;
 
 /**
  * Created by apple on 2018/12/7.
@@ -19,6 +23,22 @@ public class MainTest {
     @Test
     public void test2() {
         new TestHashFunc().test1();
+    }
+
+    @Test
+    public void test3() {
+        int len =8;
+        int[] initArra = new int[len] ;
+        for (int i = 0; i < len; i++) {
+            initArra[i] = new Random().nextInt(100);
+        }
+
+        int[] ints = new TestMergeSort().mergeSort(initArra);
+//        int[] ints = new TestMergeSort().mergeSort(new int[]{76, 91, 68});
+        System.out.println("数据长度: " + ints.length);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(StringUtils.leftPad(i+"", 8, " ") + "\t" + StringUtils.leftPad(ints[i] +"" ,8," "));
+        }
     }
 
     @Test
