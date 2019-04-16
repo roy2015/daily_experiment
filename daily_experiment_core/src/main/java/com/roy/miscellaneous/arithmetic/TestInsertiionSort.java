31f4e987b3fe99c81db19aec3baa4ccdcd7f9b2b
roy2015a <roy2011@163.com>
2019-04-16 18:24:10 +0800

    20190415 提交一个泛型的测试用例 , 数字类型的元素 add入list, list按排序存储。。。。

---
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/arithmetic/TestInsertiionSort.java
@@ -0,0 +1,57 @@
+package com.roy.miscellaneous.arithmetic;
+
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by apple on 2019/4/11.
+ *
+ * 测试插入排序 O(n^2)
+ */
+public class TestInsertiionSort {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TestInsertiionSort.class);
+
+    public  void print(int[] arry ) {
+        for (int i : arry) {
+            logger.debug("{}", i);
+        }
+        logger.debug("==================");
+    }
+
+    /**
+     *
+     * @param arry
+     *
+     *  3,1,2,9,87,8,10
+     * 1,3,2,9,87,8,10
+     */
+    public void testInsertionSort(int[] arry) {
+        int length = arry.length;
+        for (int i = 1; i < length; i++) {
+            int tmp = arry[i];
+            int j ;
+            for (j = i - 1; j >= 0; j--) {
+                if (arry[i] >= arry[j] ) {
+                    break;
+                }
+            }
+            if (j == -1) {//在列表里最小
+                for (int k = i ; k > j +1  ; k--) {
+                    arry[k] = arry[k -1];
+                }
+                arry[j + 1] = tmp;
+            } else {
+                if (i != j + 1) {//非最大 k  = j+2
+                    for (int k = i ; k >= j +2 ; k--) {
+                        arry[k] = arry[k -1];
+                    }
+                    arry[j + 1] = tmp;
+                }
+
+
+            }
+
+
+        }
+    }
+
+}
--- daily_experiment_core/src/test/java/com/roy/maintest/MainTest.java
+++ daily_experiment_core/src/test/java/com/roy/maintest/MainTest.java
@@ -1,11 +1,17 @@
 package com.roy.maintest;
 
 import com.roy.miscellaneous.*;
+import com.roy.miscellaneous.arithmetic.TestInsertiionSort;
 import com.roy.miscellaneous.arithmetic.TestMergeSort;
+import com.roy.miscellaneous.interview.TestInsertionSortWithAyyayList;
 import com.roy.miscellaneous.targetObject.TestString;
 import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.Test;
 
+import javax.naming.OperationNotSupportedException;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.Random;
 
 /**
@@ -37,6 +43,54 @@ public class MainTest {
         for (int i = 0; i < ints.length; i++) {
             System.out.println(StringUtils.leftPad(i+"", 8, " ") + "\t" + StringUtils.leftPad(ints[i] +"" ,8," "));
         }
+    }
+
+    @Test
+    //插入排序
+    public void testInsertionSort() {
+        int len =10;
+        int[] initArra = new int[len] ;
+        for (int i = 0; i < len; i++) {
+            initArra[i] = new Random().nextInt(100);
+        }
+//        int[] initArra = new int[]{41, 0, 0} ;
+        new TestInsertiionSort().print(initArra);
+        new TestInsertiionSort().testInsertionSort(initArra);
+
+        new TestInsertiionSort().print(initArra);
+    }
+
+    @Test
+    //面试提，依次add 4,6,1到 arrayList，arrayList是 1，4，6， 用到是是add(index, value)，插入排序
+    public void testInsertionSortUseArrayList() throws OperationNotSupportedException {
+        int len =7;
+        Long[] longArra = new Long[len] ;
+        for (int i = 0; i < len; i++) {
+            longArra[i] = Long.valueOf(new Random().nextInt(100));
+        }
+        //写死的数组，上面是随机数数组
+//        Long[] longArra = new Long[]{41l, 0l, 0l} ;
+
+        TestInsertionSortWithAyyayList<Long> testInsertionSortWithAyyayList = new TestInsertionSortWithAyyayList<>();
+        testInsertionSortWithAyyayList.printArray(longArra);//元素数据
+        testInsertionSortWithAyyayList.testInsertionSortUseArrayList(longArra);//process排序
+        testInsertionSortWithAyyayList.printList(testInsertionSortWithAyyayList.getList());//排序后
+    }
+
+    @Test
+    //同上，不过用的是Double
+    public void testInsertionSortUseArrayListDouble() throws OperationNotSupportedException {
+        int len =7;
+        Double[] doubleArra = new Double[len] ;
+        for (int i = 0; i < len; i++) {
+            doubleArra[i] = new BigDecimal( new Random().nextDouble()).
+                    multiply(new BigDecimal(100d), new MathContext(5, RoundingMode.HALF_UP)).doubleValue();
+        }
+
+        TestInsertionSortWithAyyayList<Double> testInsertionSortWithAyyayList = new TestInsertionSortWithAyyayList<>();
+        testInsertionSortWithAyyayList.printArray(doubleArra);//元素数据
+        testInsertionSortWithAyyayList.testInsertionSortUseArrayList(doubleArra);//process排序
+        testInsertionSortWithAyyayList.printList(testInsertionSortWithAyyayList.getList());//排序后
     }
 
     @Test
