772b740eb2a9d971020dbbc432e37d4547797d35
roy2015a <roy2011@163.com>
2019-10-31 10:17:24 +0800

    提交leecode代码 20191031

--- daily_experiment_core/src/main/java/com/roy/miscellaneous/leetcode/TestSolution258.java
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/leetcode/TestSolution258.java
@@ -43,7 +43,7 @@ public class TestSolution258 {
     }
 
     public static void main(String[] args) {
-        logger.info("{}", new Solution().addDigits(0));
+        logger.info("{}", new Solution().addDigits(18));
     }
 
 }
