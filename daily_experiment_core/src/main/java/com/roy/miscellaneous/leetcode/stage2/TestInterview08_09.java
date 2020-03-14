2e8ee6105be491c282083461cab7555151d35604
roy2015a <roy2011@163.com>
2020-03-14 20:42:45 +0800

    提交20200314提交一个中等难度的题解括号解析

---
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/leetcode/stage2/TestInterview08_09.java
@@ -0,0 +1,132 @@
+package com.roy.miscellaneous.leetcode.stage2;
+
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author guojun
+ * @date 2020/1/21 10:40
+ *
+ * 括号。设计一种算法，打印n对括号的所有合法的（例如，开闭一一对应）组合。
+
+说明：解集不能包含重复的子集。
+
+例如，给出 n = 3，生成结果为：
+
+[
+"((()))",
+"(()())",
+"(())()",
+"()(())",
+"()()()"
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/bracket-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class TestInterview08_09 {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TestInterview08_09.class);
+
+
+    static class Solution {
+        /**
+         * 递归+循环+栈
+         *
+         * 执行结果：通过
+         * 显示详情
+         * 执行用时 :165 ms, 在所有 Java 提交中击败了5.15%的用户
+         * 内存消耗 :42.1 MB, 在所有 Java 提交中击败了100.00%的用户
+         *
+         * @param n
+         * @return
+         */
+        public List<String> generateParenthesis(int n) {
+            List<String> list = new ArrayList();
+            List<String> subList;
+            if (n == 1) {
+                list.add("()");
+            } else {
+                subList = generateParenthesis( n -1);
+                String strI = null;
+                int size = subList.size();
+                for (int i=0; i< size; i++) {
+                    strI = subList.get(i);
+                    processListBaseOnSubListItem(list, strI);
+                }
+
+            }
+            return list;
+        }
+
+        /**
+         * 生成当前层次的括号，基于上一层
+         *
+         * 逻辑： 1. "（"插到i 前面，  "）"插到 j后面
+         *      2.  j== i -1 时单独处理，因为是都要插到 i前面
+         * @param retList
+         * @param subStr
+         */
+        public void processListBaseOnSubListItem(List<String> retList, String subStr) {
+            char[] chars = subStr.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                for (int j = i -1 ; j < chars.length; j++) {
+                    if (j == i -1) {//括号加到前面，单独处理
+                        String candidateStr = subStr.substring(0, j+ 1)   + "(" + ")" + subStr.substring(i);
+                        if (checkKuohao(candidateStr)) {
+                            if (!retList.contains(candidateStr)) {
+                                retList.add(candidateStr);
+                            }
+                        }
+                    } else {
+                        String candidateStr = subStr.substring(0, i) + "(" + subStr.substring(i, j + 1) +
+                                 ")" +
+                                subStr.substring(j);
+                        if (checkKuohao(candidateStr)) {
+                            if (!retList.contains(candidateStr)) {
+                                retList.add(candidateStr);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /**
+         * 校验括号的正确性
+         * @return
+         */
+        public boolean checkKuohao (String str) {
+            char[] chars = str.toCharArray();
+            Stack<String> stack = new Stack();
+            for (char aChar : chars) {
+                if (aChar == '(') {
+                    stack.push("(");
+                } else {
+                    if (stack.empty()) {
+                        return false;
+                    }
+                    stack.pop();
+                }
+            }
+            return stack.empty();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        logger.info("{}" ,solution.checkKuohao("(()))"));
+        logger.info("{}" ,solution.checkKuohao("(())"));
+        logger.info("{}" ,solution.checkKuohao("((())"));
+        logger.info("{}" ,solution.checkKuohao(")"));
+
+//        logger.info("123".substring(0,0));
+
+        List<String> list = solution.generateParenthesis(4);
+        logger.info("{}", list);
+    }
+}
