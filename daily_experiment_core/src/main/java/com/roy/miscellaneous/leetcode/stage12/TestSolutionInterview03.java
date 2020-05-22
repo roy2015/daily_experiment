e8c424084ad98224d9b9e44bb1c3b9054524d727
roy2015a <roy2011@163.com>
2020-05-22 15:42:19 +0800

    提交20200522，+4

---
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/leetcode/stage12/TestSolutionInterview03.java
@@ -0,0 +1,63 @@
+package com.roy.miscellaneous.leetcode.stage12;
+
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author guojun
+ * @date 2020/5/22
+ *
+ *面试题03. 数组中重复的数字
+找出数组中重复的数字。
+
+
+在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+
+示例 1：
+
+输入：
+[2, 3, 1, 0, 2, 5, 3]
+输出：2 或 3
+
+
+限制：
+
+2 <= n <= 100000
+ *
+ */
+public class TestSolutionInterview03 {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TestSolutionInterview03.class);
+
+    static class Solution {
+        /**
+         * 
+         *
+         *
+         * 执行结果：
+         通过
+         显示详情
+         执行用时 :1 ms, 在所有 Java 提交中击败了92.16%的用户
+         内存消耗 :47.3 MB, 在所有 Java 提交中击败了100.00%的用户
+         * @param nums
+         * @return
+         */
+        public int findRepeatNumber(int[] nums) {
+            int len = nums.length;
+            int[] bitSet = new int[len];
+            for(int i = 0; i< len; i++) {
+                int val = nums[i];
+                int existVal = bitSet[val];
+                if (existVal == 1) {
+                    return val;
+                } else{
+                    bitSet[val] =1;
+                }
+
+            }
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        logger.info("{}", new Solution());
+    }
+}
---
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/leetcode/stage12/TestSolutionInterview0803.java
@@ -0,0 +1,54 @@
+package com.roy.miscellaneous.leetcode.stage12;
+
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author guojun
+ * @date 2020/5/22
+ *
+ * 面试题 08.03. 魔术索引
+魔术索引。 在数组A[0...n-1]中，有所谓的魔术索引，满足条件A[i] = i。给定一个有序整数数组，编写一种方法找出魔术索引，若有的话，在数组A中找出一个魔术索引，如果没有，则返回-1。若有多个魔术索引，返回索引值最小的一个。
+
+示例1:
+
+输入：nums = [0, 2, 3, 4, 5]
+输出：0
+说明: 0下标的元素为0
+示例2:
+
+输入：nums = [1, 1, 1]
+输出：1
+提示:
+
+nums长度在[1, 1000000]之间
+ *
+ */
+public class TestSolutionInterview0803 {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TestSolutionInterview0803.class);
+
+    static class Solution {
+        /**
+         *
+         * 执行结果：通过显示详情
+         执行用时 :0 ms, 在所有 Java 提交中击败了100.00%的用户
+         内存消耗 :40.7 MB, 在所有 Java 提交中击败了100.00%的用户
+         *
+         * @param nums
+         * @return
+         */
+        public int findMagicIndex(int[] nums) {
+            int len = nums.length;
+            int i;
+            for(i = 0 ; i < len ; i++) {
+                if(nums[i]==i) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        logger.info("{}", new Solution().findMagicIndex(new int[]{0, 2, 3, 4, 5}));
+    }
+}
