7ccf476a0f9840a4f74db17d79cc93a3b52eee19
roy2015a <roy2011@163.com>
2020-08-22 10:43:12 +0800

    提交2020.08.22，提交21号解答的题目

--- daily_experiment_core/src/main/java/com/roy/miscellaneous/leetcode/stage1/stage10/TestSolutionInterview1712.java
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/leetcode/stage1/stage10/TestSolutionInterview1712.java
@@ -41,51 +41,153 @@ public class TestSolutionInterview1712 {
     static class Solution {
         private TreeNode newHead ;
 
-        public TreeNode convertBiNode(TreeNode root) {
-            TreeNode retNode = root;
+        /**
+         *
+         *
+         *
+         * 思路：
+         *  1. 每次递归，left sub-tree应该返回最大的节点， right sub-tree应该返回最小的节点， 再挂上当前节点
+         *  2. 新的root应该单独计算作为class的field
+         *还是比较紊乱
+         *
+         * @param root
+         * @return
+         */
+        public TreeNode convertBiNode1(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+
             TreeNode leftNode = root.left;
             TreeNode rightNode = root.right;
 
+            //降低复杂度，从root开始一次
             if(leftNode != null) {
-                TreeNode lefChildRt = fun(leftNode);
-                retNode = lefChildRt;
-                retNode.right = root;
+                TreeNode lefChildRt = funLeft(leftNode, 1, 1);
+                lefChildRt.right = root;
+                root.left = null;
             }
 
             if(rightNode != null) {
-                TreeNode rightChildRt = fun(rightNode);
+                TreeNode rightChildRt = funLeft(rightNode, 2,2);
                 root.right = rightChildRt;
             }
-            return retNode;
+
+            //考虑树只有一个节点的情况
+            return newHead == null ? root : newHead ;
         }
 
-        public TreeNode fun(TreeNode node) {
+
+
+
+
+        public TreeNode funLeft(TreeNode node, int currentFlag, int preFlag) {
             TreeNode left = node.left;
             TreeNode right = node.right;
 
-            TreeNode retNode = node;
-            node.left = null;
+            TreeNode funRight = null;
+            TreeNode funLeft = null;
+            TreeNode retNode = node;//默认返回本节点
+//            node.left = null;
             if (left == null) {
                 if (newHead == null) {
                     newHead = node;
                     newHead.left = null;
                 }
             } else {
-                TreeNode funLeft = fun(left);
-                funLeft.left = null;//funLeft should not-null
+                funLeft = funLeft(left, 1, preFlag);
+                funLeft.left = null;//funLeft can be not-null
                 funLeft.right = node;
+                node.left = null;
             }
 
             if (right != null) {
-                TreeNode funRight = fun(right);
-                node.right = node;
-                retNode = funRight;
+                funRight = funLeft(right, 2, preFlag);
+                node.right = funRight;
+            } else {}
+
+            if (preFlag == 1 && currentFlag == 1) {
+                if (funRight != null) {
+                    retNode = funRight;
+                }
+            } else if (preFlag == 1 && currentFlag == 2) {
+                if (funLeft != null) {
+                    retNode = funLeft;
+                }
+            } else if (preFlag == 2 && currentFlag == 1 ) {
+                if (funLeft != null) {
+                    retNode = funLeft;
+                }
+            } else {
+                if (funLeft != null) {
+                    retNode = funLeft;
+                }
             }
+
             return retNode;
+        }
+
+
+        public TreeNode convertBiNode(TreeNode root) {
+            TreeNode dummyNode = new TreeNode(-1);
+            convertBiNodeSub(root, dummyNode);
+            return dummyNode.right;
+        }
+
+        /**
+         *
+         *
+         * 参考了题解，用中序遍历并记录上一个节点（层次节点）
+         *
+         *执行结果：
+         * 通过
+         * 显示详情
+         * 执行用时：
+         * 0 ms
+         * , 在所有 Java 提交中击败了
+         * 100.00%
+         * 的用户
+         * 内存消耗：
+         * 45.7 MB
+         * , 在所有 Java 提交中击败了
+         * 13.08%
+         * 的用户
+         *
+         * @param calNode
+         * @param currentNode
+         */
+        private TreeNode convertBiNodeSub(TreeNode calNode, TreeNode currentNode) {
+            if (calNode == null) {
+                return null;
+            }
+            TreeNode leftRetNode = convertBiNodeSub(calNode.left, currentNode);
+            if (leftRetNode != null) {
+                currentNode = leftRetNode;
+            }
+
+            currentNode.right = calNode;//只想当前节点
+            calNode.left = null;//用完失效left
+            currentNode = calNode;
+            //到右边去
+            TreeNode rihgtRetNode = convertBiNodeSub(calNode.right, currentNode);
+            if (rihgtRetNode != null) {
+                currentNode = rihgtRetNode;
+            }
+            return currentNode;
         }
     }
 
     public static void main(String[] args) {
-        logger.info("{}", new Solution());
+
+
+        TreeNode node1 = new TreeNode(4);
+        node1.left = new TreeNode(2);
+        node1.left.left = new TreeNode(1);
+        node1.left.left.left = new TreeNode(0);
+        node1.left.right = new TreeNode(3);
+
+        node1.right = new TreeNode(5);
+        node1.right.right = new TreeNode(6);
+        logger.info("{}", new Solution().convertBiNode(node1));
     }
 }
---
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/leetcode/stage1/stage11/TestSolutionInterview1005.java
@@ -0,0 +1,91 @@
+package com.roy.miscellaneous.leetcode.stage1.stage11;
+
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author guojun
+ * @date 2020/8/20
+ *
+ *面试题 10.05. 稀疏数组搜索
+ * 稀疏数组搜索。有个排好序的字符串数组，其中散布着一些空字符串，编写一种方法，找出给定字符串的位置。
+ *
+ * 示例1:
+ *
+ *  输入: words = ["at", "", "", "", "ball", "", "", "car", "", "","dad", "", ""], s = "ta"
+ *  输出：-1
+ *  说明: 不存在返回-1。
+ * 示例2:
+ *
+ *  输入：words = ["at", "", "", "", "ball", "", "", "car", "", "","dad", "", ""], s = "ball"
+ *  输出：4
+ * 提示:
+ *
+ * words的长度在[1, 1000000]之间
+ *
+ */
+public class TestSolutionInterview1005 {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TestSolutionInterview1005.class);
+
+
+
+
+    static class Solution {
+        /**
+         * 思路：
+         *  1. 把空字符串过滤掉，换成无空值的递增数值，然后走正常的二分查找 -------- 不可取，是逃避问题
+         *  2.  强行二分查找 --- 可取，不过要额外处理空字符串
+         *
+         *
+         * 执行结果：
+         * 通过
+         * 显示详情
+         * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+         * 内存消耗：39.7 MB, 在所有 Java 提交中击败了43.55%的用户
+         *
+         * @param words
+         * @param s
+         * @return
+         */
+        public int findString(String[] words, String s) {
+            int len = words.length;
+            int low = 0;
+            int high = len -1;
+            int mid;
+            while (low <= high) {
+                mid = (low + high) >>>1;
+                //向左找最近非空的字符串
+                int copyMid = mid;
+                /*写了个大坑 s == "" 应该走equals() */
+                while (words[copyMid]/*==""*/.equals("") && copyMid >= low ) {
+                    copyMid --;
+                }
+                if (copyMid < low) {//全部是空串
+                    low = mid +1;
+                } else {
+                    if (words[copyMid].compareTo(s) == 0) {
+                        return copyMid;
+                    } else if (words[copyMid].compareTo(s) > 0) {
+                        high = copyMid -1;
+                    } else {
+                        low = mid + 1;
+                    }
+                }
+            }
+            return -1;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        logger.info("{}", new Solution().findString(new String[]
+                        {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""},
+                "at"));//0
+
+
+        logger.info("{}", new Solution().findString(new String[]
+                {"at", "", "", "", "ball", "", "", "car", "", "","dad", "", ""},
+                "ball"));//4
+
+    }
+}
