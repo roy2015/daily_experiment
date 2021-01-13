3004cecff6b78ef8e027c937652a76b5cc9a4b80
roy2015a <roy2011@163.com>
2021-01-13 18:06:16 +0800

    提交2021.01.13 提交MultiMap

---
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/guava/event/Animal.java
@@ -0,0 +1,15 @@
+package com.roy.miscellaneous.guava.event;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * @author guojun
+ * @date 2021/1/13 下午3:52
+ */
+@Data
+@Accessors(chain = true)
+public class Animal {
+    private String name;
+    private int age;
+}
--- daily_experiment_core/src/main/java/com/roy/miscellaneous/guava/event/TestEventBus.java
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/guava/event/TestEventBus.java
@@ -1,11 +1,16 @@
 package com.roy.miscellaneous.guava.event;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
+import com.roy.miscellaneous.protoBuf.AddressBookProtos;
+import com.roy.miscellaneous.targetObject.TestVO;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
@@ -15,12 +20,12 @@ import java.util.concurrent.TimeUnit;
  */
 public class TestEventBus {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args)  {
         EventBus eventBus = new AsyncEventBus(Executors.newFixedThreadPool(10));
 //        EventBus eventBus = new EventBus("123");
         eventBus.register(new CustomSubscriber());
-        eventBus.register(new CustomSubscriber1());
-        eventBus.register(new CustomSubscriber2());
+//        eventBus.register(new CustomSubscriber1());
+//        eventBus.register(new CustomSubscriber2());
 
         for (int i = 1; i <= 10; i++) {
             eventBus.post(new CustomEvent().setEventId(i ));
