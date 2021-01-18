6f60c4fd457d86e3b720240becaf1c7496096586
roy2015a <roy2011@163.com>
2021-01-18 16:10:13 +0800

    提交2021.01.18 学习Base64编码过程

--- daily_experiment_core/src/main/java/com/roy/miscellaneous/arithmetic/TestBase64Codec.java
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/arithmetic/TestBase64Codec.java
@@ -9,8 +9,13 @@ import java.nio.charset.Charset;
 /**
  * Created by apple on 2019/8/6.
  *
+ *  Base64编码要求把3个8位字节（3*8=24）转化为4个6位的字节（4*6=24），之后在6位的前面补两个0，形成8位一个字节的形式。
+ *  如果剩下的字符不足3个字节，则用0填充，输出字符使用'='，因此编码后输出的文本末尾可能会出现1或2个'='。
+ *  Base64编码后的长度不固定，如果原文是3n byte(不足3补)(3*8*n bit => 4*6*n bit => 4*8*n bit, 4n个字节/字符), 3n个字节=》4n个字节
+ *  原文线性增长三分之一,
+ *  举例：比如Base64编码MD5后的byte[] a, a是16byte, 16约等于18, 18+18*(1/3)=24
  *
- *  Base64编码后的长度不固定, 和原文长度成线性增长
+ *  总结： 3的整数个字符BASE64后不会出现=
  *
  */
 public class TestBase64Codec {
@@ -21,9 +26,10 @@ public class TestBase64Codec {
      * @param src
      */
     public static void testBase64Encode(String src) throws UnsupportedEncodingException {
-        byte[] bytes = Base64.encodeBase64(src.getBytes("utf-8"));
-        logger.info("base64编码前 ：{}", src);
-        logger.info("base64编码后：{}", new String(bytes, "utf-8"));
+        byte[] srcBytes = src.getBytes(Charset.forName("UTF-8"));
+        byte[] encodeBytes = Base64.encodeBase64(srcBytes);
+        logger.info("base64编码前 ：{}, length:{}", src, srcBytes.length);
+        logger.info("base64编码后：{}, length:{}",new String(encodeBytes, "utf-8"), encodeBytes.length);
     }
 
     /**
@@ -34,6 +40,11 @@ public class TestBase64Codec {
         logger.info("base64解码前：{}", src);
         byte[] bytes = Base64.decodeBase64(src);
         logger.info("base64解码后：{}", new String(bytes, "utf-8"));
+    }
+
+    public static void main(String[] args) throws Exception {
+        testBase64Encode("0123456789000000");
+        testBase64Decode("MTIzNDU2");
     }
 
 }
