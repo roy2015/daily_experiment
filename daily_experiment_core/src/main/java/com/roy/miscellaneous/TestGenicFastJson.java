ee811b90b6a061aef3eb71605736f55458288bf7
roy2015a <roy2011@163.com>
2021-03-30 10:46:46 +0800

    提交2021.03.30

--- daily_experiment_core/src/main/java/com/roy/miscellaneous/TestGenicFastJson.java
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/TestGenicFastJson.java
@@ -1,10 +1,9 @@
 package com.roy.miscellaneous;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
-import com.roy.miscellaneous.targetObject.PCNotiWebsocketMsg;
-import com.roy.miscellaneous.targetObject.UserVO;
+import com.roy.miscellaneous.targetObject.*;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -29,10 +28,7 @@ public class TestGenicFastJson {
         websocketMsg.setData(userVO);
         websocketMsg.setMessageCategory("123");
         websocketMsg.setMessageLevel("1");
-
         logger.debug(JSON.toJSONString(websocketMsg));
-
-
     }
 
     public void testGenicFastJsonStrToObj() {
@@ -40,7 +36,61 @@ public class TestGenicFastJson {
         PCNotiWebsocketMsg obj = JSON.parseObject(str, new TypeReference<PCNotiWebsocketMsg<UserVO>>(){});
         logger.debug(obj.toString());
 
+    }
+
+    public static void testGenicFastJsonStr () {
+
+        UserVO userVO = new UserVO();
+        userVO.setUserId(123);
+        userVO.setUserSex("nan");
+        userVO.setUserName("roy");
 
 
+
+        User1VO user1VO = new User1VO();
+        User2VO user2VO = new User2VO();
+        BeanUtil.copyProperties(userVO, user1VO);
+        BeanUtil.copyProperties(userVO, user2VO);
+        user1VO.setUserId(1231);
+        user2VO.setUserId(1232);
+
+        PCNotiWebsocketMsgCopy<UserVO, User1VO, User2VO> websocketMsg = new PCNotiWebsocketMsgCopy<>();
+        websocketMsg.setData(userVO);
+        websocketMsg.setData1(user1VO);
+        websocketMsg.setData2(user2VO);
+        websocketMsg.setMessageCategory("123");
+        websocketMsg.setMessageLevel("1");
+
+        logger.debug(JSON.toJSONString(websocketMsg, true));
+    }
+
+    public static void testGenicFastJsonStrToObj1() {
+        String str = "{\n" +
+                "\t\"data\":{\n" +
+                "\t\t\"userId\":123,\n" +
+                "\t\t\"userName\":\"roy\",\n" +
+                "\t\t\"userSex\":\"nan\"\n" +
+                "\t},\n" +
+                "\t\"data1\":{\n" +
+                "\t\t\"userId\":1231,\n" +
+                "\t\t\"userName\":\"roy\",\n" +
+                "\t\t\"userSex\":\"nan\"\n" +
+                "\t},\n" +
+                "\t\"data2\":{\n" +
+                "\t\t\"userId\":1232,\n" +
+                "\t\t\"userName\":\"roy\",\n" +
+                "\t\t\"userSex\":\"nan\"\n" +
+                "\t},\n" +
+                "\t\"messageCategory\":\"123\",\n" +
+                "\t\"messageLevel\":\"1\"\n" +
+                "}";
+        PCNotiWebsocketMsgCopy<UserVO, User1VO, User2VO> obj = JSON.parseObject(str, new TypeReference<PCNotiWebsocketMsgCopy<UserVO, User1VO, User2VO>>(UserVO.class, User1VO.class, User2VO.class) {
+        });
+        logger.debug(obj.toString());
+
+    }
+
+    public static void main(String[] args) {
+        testGenicFastJsonStrToObj1();
     }
 }
