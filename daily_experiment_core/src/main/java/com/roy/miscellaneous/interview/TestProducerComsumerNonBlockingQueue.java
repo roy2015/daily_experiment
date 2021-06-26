9295a32e890035636f4a13f5f09c0d1fac057bd7
roy2015a <roy2011@163.com>
2021-06-26 21:54:57 +0800

    提交2021.06.26 提交一波代码面试用

---
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/interview/TestProducerComsumerNonBlockingQueue.java
@@ -0,0 +1,113 @@
+package com.roy.miscellaneous.interview;
+
+import lombok.Data;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author guojun
+ * @date 2021/6/11
+ */
+public class TestProducerComsumerNonBlockingQueue {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TestProducerComsumerNonBlockingQueue.class);
+
+
+    static class Solution {
+
+        private ReentrantLock lock;
+        private Condition producerCondition;
+        private Condition consumerCondition;
+        private volatile boolean fullFlag;
+        private volatile String diamond;
+
+        public Solution() {
+
+        }
+
+        public void start() throws IOException {
+            lock = new ReentrantLock();
+            producerCondition = lock.newCondition();
+            consumerCondition = lock.newCondition();
+            fullFlag = false;
+
+            Producer producer = new Producer();
+            Consumer consumer = new Consumer();
+
+
+            new Thread(producer).start();
+            new Thread(consumer).start();
+
+            System.in.read();
+        }
+
+        @Data
+        public class Producer implements Runnable {
+
+            @Override
+            public void run() {
+                try {
+                    int idx = 0;
+                    for (int i =0; i< 10 ;i++ ) {
+
+                        lock.lock();  // block until condition holds
+                        try {
+                            if (fullFlag) {
+                                consumerCondition.await();
+                            } else {//未满
+                                String toAdd = "A" + (idx++);
+                                logger.info("add {}", toAdd);
+                                diamond = toAdd;
+                                fullFlag = true;
+                                producerCondition.signal();
+                            }
+
+                        } finally {
+                            lock.unlock();
+                        }
+
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @Data
+        public class Consumer implements Runnable {
+            private String workerName;
+            private int loopIdx = 1;//初始化1
+
+            @Override
+            public void run() {
+                try {
+                    for (int i =0; i< 10 ;i++ ) {
+                        lock.lock();  // block until condition holds
+                        try {
+                            if (!fullFlag) {
+                                producerCondition.await();
+                            } else {
+                                logger.info("      get {}", diamond);
+                                diamond = "";
+                                fullFlag = false;
+                                consumerCondition.signal();
+                            }
+
+                        } finally {
+                            lock.unlock();
+                        }
+
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new TestProducerComsumerNonBlockingQueue.Solution().start();
+    }
+}
---
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/interview/TestProducerComsumerWithBlockingQueue.java
@@ -0,0 +1,70 @@
+package com.roy.miscellaneous.interview;
+
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ *
+ * 生产者消费者
+ * 使用BlockingQueue
+ *
+ * @author guojun
+ * @date 2021/6/11
+ */
+public class TestProducerComsumerWithBlockingQueue {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TestProducerComsumerWithBlockingQueue.class);
+
+
+    static class Solution {
+        private BlockingQueue<String> queue = new ArrayBlockingQueue<> (5, true);
+
+        public void test () throws IOException, InterruptedException {
+            queue.put("A1");
+            queue.put("A2");
+            queue.put("A3");
+
+            new Thread(new Producer()).start();
+            new Thread(new Consumer()).start();
+
+            System.in.read();
+
+        }
+
+        public class Producer implements Runnable {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    String toAdd = "B" + i;
+                    try {
+                        queue.put(toAdd);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    logger.info("       add {}" , toAdd);
+                }
+            }
+        }
+
+        public class Consumer implements Runnable {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    try {
+                        logger.info("get {}", queue.take());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Solution().test();
+    }
+}
