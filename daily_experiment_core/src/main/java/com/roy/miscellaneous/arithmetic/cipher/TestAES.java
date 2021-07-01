df7cc96bc998365f3eee45bcd2b9dbf4b15669f9
roy2015a <roy2011@163.com>
2021-07-01 22:49:16 +0800

    提交2021.07.01 71提交代码，献给党

--- daily_experiment_core/src/main/java/com/roy/miscellaneous/arithmetic/cipher/TestAES.java
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/arithmetic/cipher/TestAES.java
@@ -1,14 +1,11 @@
 package com.roy.miscellaneous.arithmetic.cipher;
 
-import com.sun.xml.internal.rngom.parse.host.Base;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
 import org.slf4j.LoggerFactory;
 
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
@@ -29,52 +26,47 @@ public enum TestAES {
     SINGLETON;
 
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TestAES.class);
-    private static final String SECRECT = "passwrod";
-    private String secretKeyHexString = "8c3f9fe5e8b1641790b51de9f1f22f49";
+    private static final String SECRET_KEY_SEED = "passwrod";
+    private String secretKeyBase64String ;
 
-    public String wrap(String keyString) throws Exception {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        //初始化密钥生成器，指定密钥长度为128，指定随机源的种子为指定的密钥(这里是"passward")
-        keyGenerator.init(128, new SecureRandom(SECRECT.getBytes()));
-        SecretKey secretKey = keyGenerator.generateKey();
-        secretKeyHexString = Hex.encodeHexString(secretKey.getEncoded());
-        logger.info("生成的秘钥[HEX编码]为: {}", secretKeyHexString);
-        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getEncoded(), "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.WRAP_MODE, secretKeySpec);
-        SecretKeySpec key = new SecretKeySpec(keyString.getBytes(), "AES");
-        byte[] bytes = cipher.wrap(key);
-        return Hex.encodeHexString(bytes);
+    private String secretKeyBase64Encode ;
+
+    /**
+     * base64编码后的secret_key,  AES必须是16位字节数组
+     * 所以是32位(24扩大三分之一)
+     *
+     * java 原生jdk默认只有AES128，生成128位的对称密钥
+     */
+    private static String SECRET_KEY = "D7BclY7N98ONvsRW+a+Tyw==";//24*3/4=16
+//    private static String SECRET_KEY = "D7BclY7N98ONvsRW+a+Ty61b/X73xfqs";//32*3/4=24
+    private static String KEY_ALGORITHM = "AES";
+    private static String CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";
+
+
+    public String initSecretKey() {
+        KeyGenerator keyGenerator = null;
+        try {
+            keyGenerator = KeyGenerator.getInstance(KEY_ALGORITHM);
+            //初始化密钥生成器，128可以不指定，指定密钥长度为128，指定随机源的种子为指定的密钥(这里是"passward")
+            keyGenerator.init(new SecureRandom(SECRET_KEY_SEED.getBytes()));//SECRET_KEY_SEED密钥种子指定时，secretKey.getEncoded()固定，不传secretKey.getEncoded()就随机
+            SecretKey secretKey = keyGenerator.generateKey();
+            secretKeyBase64Encode = Base64.encodeBase64String(secretKey.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return secretKeyBase64Encode;
     }
- 
-    public String unwrap(String keyString) throws Exception {
-        byte[] rawKey = Hex.decodeHex(keyString.toCharArray());
-        byte[] secretKeyBytes = Hex.decodeHex(secretKeyHexString.toCharArray());
-        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKeyBytes, "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.UNWRAP_MODE, secretKeySpec);
-        SecretKey key = (SecretKey) cipher.unwrap(rawKey, "AES", Cipher.SECRET_KEY);
-        return new String(key.getEncoded());
-    }
+
+
 
     public byte[] doEncrypt(String srcStr) {
         Cipher cipher = null;
         try {
-            cipher = Cipher.getInstance("AES");
-            SecretKeySpec key = new SecretKeySpec(Base64.decodeBase64("QeYT66chJxxKkPhQiyO5ZQ=="), "AES");
+            cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+            SecretKeySpec key = new SecretKeySpec(Base64.decodeBase64(SECRET_KEY), KEY_ALGORITHM);
             cipher.init(Cipher.ENCRYPT_MODE, key);
             return cipher.doFinal(srcStr.getBytes("utf-8"));
-        } catch (NoSuchAlgorithmException e) {
-            logger.error(e.getMessage(), e);
-        } catch (NoSuchPaddingException e) {
-            logger.error(e.getMessage(), e);
-        } catch (UnsupportedEncodingException e) {
-            logger.error(e.getMessage(), e);
-        } catch (InvalidKeyException e) {
-            logger.error(e.getMessage(), e);
-        } catch (BadPaddingException e) {
-            logger.error(e.getMessage(), e);
-        } catch (IllegalBlockSizeException e) {
+        } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
         return null;
@@ -83,31 +75,53 @@ public enum TestAES {
     public byte[] doDecrypt(String srcStr) {
         Cipher cipher = null;
         try {
-            cipher = Cipher.getInstance("AES");
-            SecretKeySpec key = new SecretKeySpec(Base64.decodeBase64("QeYT66chJxxKkPhQiyO5ZQ=="), "AES");
+            cipher = Cipher.getInstance(KEY_ALGORITHM);
+            SecretKeySpec key = new SecretKeySpec(Base64.decodeBase64(SECRET_KEY), KEY_ALGORITHM);
             cipher.init(Cipher.DECRYPT_MODE, key);
             return cipher.doFinal(Base64.decodeBase64(srcStr));
-        } catch (NoSuchAlgorithmException e) {
-            logger.error(e.getMessage(), e);
-        } catch (NoSuchPaddingException e) {
-            logger.error(e.getMessage(), e);
-        } catch (InvalidKeyException e) {
-            logger.error(e.getMessage(), e);
-        } catch (BadPaddingException e) {
-            logger.error(e.getMessage(), e);
-        } catch (IllegalBlockSizeException e) {
+        } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
         return null;
     }
+
+    public String wrap(String keyString) throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(KEY_ALGORITHM);
+        //初始化密钥生成器，指定密钥长度为128, AES只能128， DES3只能192
+        keyGenerator.init(new SecureRandom(SECRET_KEY_SEED.getBytes()));
+        SecretKey secretKey = keyGenerator.generateKey();
+        secretKeyBase64String = Base64.encodeBase64String(secretKey.getEncoded());
+        logger.info("生成的秘钥[decode编码]为: {}", secretKeyBase64String);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getEncoded(), KEY_ALGORITHM);
+        Cipher cipher = Cipher.getInstance(KEY_ALGORITHM);
+        cipher.init(Cipher.WRAP_MODE, secretKeySpec);
+        SecretKeySpec key = new SecretKeySpec(keyString.getBytes(), KEY_ALGORITHM);
+        byte[] bytes = cipher.wrap(key);
+        return Base64.encodeBase64String(bytes);
+    }
+
+    public String unwrap(String keyString) throws Exception {
+        byte[] rawKey = Base64.decodeBase64(keyString);
+        byte[] secretKeyBytes = Base64.decodeBase64(secretKeyBase64String);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKeyBytes, KEY_ALGORITHM);
+        Cipher cipher = Cipher.getInstance(KEY_ALGORITHM);
+        cipher.init(Cipher.UNWRAP_MODE, secretKeySpec);
+        SecretKey key = (SecretKey) cipher.unwrap(rawKey, KEY_ALGORITHM, Cipher.SECRET_KEY);
+        return new String(key.getEncoded());
+    }
  
     public static void main(String[] args) throws Exception {
-//        String wrapKey = TestAES.SINGLETON.wrap("doge");
-//        logger.info(wrapKey);
-        logger.info(TestAES.SINGLETON.unwrap("16556ad39dcd597344eb7047859f8b01"));
+        String wrapKey = TestAES.SINGLETON.wrap("doge");
+        logger.info("wrap {}",wrapKey);
+        logger.info("unwrap {}",TestAES.SINGLETON.unwrap("dwUHQhiNS5eh1AHbkCuGTQ=="));
+
+
+        logger.info(TestAES.SINGLETON.initSecretKey());
+        logger.info(TestAES.SINGLETON.initSecretKey());
+
         byte[] encrypt = TestAES.SINGLETON.doEncrypt("123");
-        logger.info("{}", new String(encrypt));
+        logger.info("encrypt {}", Base64.encodeBase64String(encrypt));
         byte[] decrypt = TestAES.SINGLETON.doDecrypt(Base64.encodeBase64String(encrypt));
-        logger.info("{}", new String(decrypt));
+        logger.info("decrypt {}", new String(decrypt));
     }
 }
--- daily_experiment_core/src/main/java/com/roy/miscellaneous/arithmetic/cipher/TestDES3.java
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/arithmetic/cipher/TestDES3.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -15,22 +16,37 @@ import java.security.SecureRandom;
  * Created by apple on 2019/8/6.
  * 对称加密
  * AES、3DES
- *
+ * <p>
  * 三重des算法（DESede）
- *
+ * <p>
  * SecretKeySpec key可以指定但必须是24位，也可以走KeyGenerator生成, initSecretKey就是后面一种
  */
 public class TestDES3 {
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TestDES3.class);
-    //SECRET_KEY: 对称秘钥  DESede必须是24位字节数组, AES必须是16位字节数组，这里被编码成String了!!注意下
-    private static String SECRET_KEY = "ZgYRBq5D1MArTx9cU/n4Ig/pQCz/LkCvc9cEj9NNHLQ=" ;
 
-    private static final String KEY_ALGORITHM = "DESede";//key 算法  AES or DESede
-    private static final String DEFAULT_CIPHER_ALGORITHM = "DESede/ECB/PKCS5Padding";// 默认的加密算法 含义：算法/工作模式/填充
+    /**
+     * SECRET_KEY: 对称秘钥  DESede必须是24位字节数组, AES必须是16位字节数组，这里被base编码成String了，
+     * 所以是32位(24扩大三分之一)
+     */
+    private static String SECRET_KEY = "MTIzMzIxMTIzNDU2NzEzNTU0Mjg3ODEz";
 
-    private SecretKeySpec key ;
+    /**
+     * key 算法  AES or DESede
+     */
+    private static final String KEY_ALGORITHM = "DESede";
 
-    private  void initSecretKey(String keyStr) {
+    /**
+     * 默认的加密算法 含义：算法/工作模式/填充
+     */
+    private static final String DEFAULT_CIPHER_ALGORITHM = "DESede/ECB/PKCS5Padding";
+
+    private SecretKeySpec key;
+
+    /**
+     * 初始化SecretKey
+     * @param keyStr
+     */
+    private void initSecretKey(String keyStr) {
         //返回生成指定算法密钥生成器的KeyGenerator 对象
         KeyGenerator kg = null;
         try {
@@ -38,18 +54,16 @@ public class TestDES3 {
             kg.init(new SecureRandom(keyStr.getBytes("utf-8")));
             //生成一个密钥
             SecretKey secretKey = kg.generateKey();
-            SECRET_KEY = Base64.encodeBase64String (secretKey.getEncoded());
+            SECRET_KEY = Base64.encodeBase64String(secretKey.getEncoded());
             logger.info("对称秘钥:{}", SECRET_KEY);
-//             转换为DESede专用密钥
-        } catch (NoSuchAlgorithmException e) {
-            logger.error(e.getMessage(), e);
-        } catch (UnsupportedEncodingException e) {
+        } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
     }
 
     /**
-     *  加密
+     * 加密
+     *
      * @param srcStr 需要加密的字符串
      * @return
      */
@@ -57,20 +71,10 @@ public class TestDES3 {
         Cipher cipher = null;
         try {
             cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
-            SecretKeySpec key = new SecretKeySpec(Base64.decodeBase64(SECRET_KEY) , KEY_ALGORITHM);
+            SecretKeySpec key = new SecretKeySpec(Base64.decodeBase64(SECRET_KEY), KEY_ALGORITHM);
             cipher.init(Cipher.ENCRYPT_MODE, key);
             return cipher.doFinal(srcStr.getBytes("utf-8"));
-        } catch (NoSuchAlgorithmException e) {
-            logger.error(e.getMessage(), e);
-        } catch (NoSuchPaddingException e) {
-            logger.error(e.getMessage(), e);
-        } catch (UnsupportedEncodingException e) {
-            logger.error(e.getMessage(), e);
-        } catch (InvalidKeyException e) {
-            logger.error(e.getMessage(), e);
-        } catch (BadPaddingException e) {
-            logger.error(e.getMessage(), e);
-        } catch (IllegalBlockSizeException e) {
+        } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
         return null;
@@ -84,39 +88,27 @@ public class TestDES3 {
             cipher.init(Cipher.DECRYPT_MODE, key);
             byte[] bytes = cipher.doFinal(srcbytes);
             return new String(bytes, "utf-8");
-        } catch (NoSuchAlgorithmException e) {
-            logger.error(e.getMessage(), e);
-        } catch (NoSuchPaddingException e) {
-            logger.error(e.getMessage(), e);
-        } catch (BadPaddingException e) {
-            logger.error(e.getMessage(), e);
-        } catch (UnsupportedEncodingException e) {
-            logger.error(e.getMessage(), e);
-        } catch (IllegalBlockSizeException e) {
-            logger.error(e.getMessage(), e);
-        } catch (InvalidKeyException e) {
+        } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
         return null;
     }
 
     public static void testDES3(String srcStr) throws UnsupportedEncodingException {
-        logger.info("源字符串为：{} ", srcStr );
+        logger.info("源字符串为：{} ", srcStr);
         TestDES3 des3 = new TestDES3();
 //        des3.initSecretKey("bg244210");
-
+        logger.info("密钥(24字节),转base64编码 {}", Base64.encodeBase64String(new String("123321123456713554287813")
+                .getBytes(Charset.forName("UTF-8"))));
         byte[] encryptBytes = des3.doEncrypt(srcStr);
-        logger.info("加密后：{} ", new String(encryptBytes, "utf-8") );
+        logger.info("加密后：{} ", new String(encryptBytes, "utf-8"));
         String encodeBase64String = Base64.encodeBase64String(encryptBytes);
         logger.info("加密后base64Encode：{} ", encodeBase64String);
         String encodeHexString = Hex.encodeHexString(encryptBytes);
         logger.info("加密后HexEncode：{} ", encodeHexString);
-        logger.info("加密后：{} ", new String(Base64.decodeBase64(encodeBase64String), "UTF-8") );
+        logger.info("加密后：{} ", new String(Base64.decodeBase64(encodeBase64String), "UTF-8"));
         logger.info("解密 {}", des3.doDecrypt(encryptBytes));
-
-
     }
-
 
 
 }
