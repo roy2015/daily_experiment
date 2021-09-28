b33fa05a09b355f36daee0ea7ba5d18e894c9ebb
guojun <guojun@yunlizhihui.com>
2021-09-28 11:40:42 +0800

    313 理解了题解后不断优化

--- daily_experiment_core/src/main/java/com/roy/miscellaneous/leetcode/stage2/stage21/TestSolution313.java
+++ daily_experiment_core/src/main/java/com/roy/miscellaneous/leetcode/stage2/stage21/TestSolution313.java
@@ -1,10 +1,14 @@
 package com.roy.miscellaneous.leetcode.stage2.stage21;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.slf4j.LoggerFactory;
 
@@ -96,22 +100,176 @@ public class TestSolution313 {
             }
         }
 
+        /**
+         * 执行用时：
+         * 1077 ms
+         * , 在所有 Java 提交中击败了
+         * 5.02%
+         * 的用户
+         * 内存消耗：
+         * 63.9 MB
+         * , 在所有 Java 提交中击败了
+         * 44.76%
+         * 的用户
+         * 通过测试用例：
+         * @param n
+         * @param primes
+         * @return
+         */
+        public int nthSuperUglyNumber1(int n, int[] primes) {
+            int length = primes.length;
+            int[] pointers = new int[length];
+            List<Integer> dp = new ArrayList<>(n);
+            dp.add(1);
+
+            int i = 1;//从1开始
+            int limit = n -1;
+            while (i <= limit) {
+                int minIdx = 0;
+                int minVal = Integer.MAX_VALUE;
+                for (int j = 0; j < length; j++) {
+                    Integer dpVal = dp.get(pointers[j]);
+                    int dpValPrimes = dpVal * primes[j];
+                    if (dpValPrimes < minVal) {
+                        minIdx = j;
+                        minVal = dpValPrimes;
+                    }
+                }
+                if (dp.get(dp.size() -1 ).compareTo(minVal) != 0) {
+                    dp.add(minVal);
+                    i ++;
+                }
+                pointers[minIdx]++;
+            }
+            return dp.get(dp.size() -1 );
+        }
+
+        /**
+         * 执行结果：
+         * 通过
+         * 显示详情
+         * 添加备注
+         *
+         * 执行用时：
+         * 598 ms
+         * , 在所有 Java 提交中击败了
+         * 9.80%
+         * 的用户
+         * 内存消耗：
+         * 39.9 MB
+         * , 在所有 Java 提交中击败了
+         * 54.90%
+         * 的用户
+         * 通过测试用例：
+         * 85 / 85
+         * @param n
+         * @param primes
+         * @return
+         */
+        public int nthSuperUglyNumber2(int n, int[] primes) {
+            int length = primes.length;
+            int[] pointers = new int[length];
+            int[] dp = new int[n];
+            dp[0] = 1;
+
+            int i = 1;//从1开始
+            int limit = n -1;
+            while (i <= limit) {
+                int minIdx = 0;
+                int minVal = Integer.MAX_VALUE;
+                for (int j = 0; j < length; j++) {
+                    int dpVal = dp[pointers[j]];
+                    int dpValPrimes = dpVal * primes[j];
+                    if (dpValPrimes < minVal) {
+                        minIdx = j;
+                        minVal = dpValPrimes;
+                    }
+                }
+                if (dp[i -1] != minVal) {
+                    dp[i] = minVal;
+                    i ++;
+                }
+                pointers[minIdx]++;
+            }
+            return dp[i -1];
+        }
 
 
+        /**
+         *
+         *
+         * 优化点：
+         * 提前检验重复可以减少外层循环，毕竟pointers元素个数和primes一致，不是长数据不耗时 pointers元素的值是dp元素的指针
+         *
+         * 执行结果：
+         * 通过
+         * 显示详情
+         * 添加备注
+         *
+         * 执行用时：
+         * 313 ms
+         * , 在所有 Java 提交中击败了
+         * 49.89%
+         * 的用户
+         * 内存消耗：
+         * 39.9 MB
+         * , 在所有 Java 提交中击败了
+         * 54.91%
+         * 的用户
+         * 通过测试用例：
+         * 85 / 85
+         *
+         * @param n
+         * @param primes
+         * @return
+         */
+        public int nthSuperUglyNumber3(int n, int[] primes) {
+            int length = primes.length;
+            int[] pointers = new int[length];
+            int[] dp = new int[n];
+            dp[0] = 1;
+
+            int i = 1;//从1开始
+            int limit = n -1;
+            while (i <= limit) {
+                int minVal = Integer.MAX_VALUE;
+                for (int j = 0; j < length; j++) {
+                    int dpValPrimes = dp[pointers[j]] * primes[j];
+                    if (dpValPrimes < minVal) {
+                        minVal = dpValPrimes;
+                    }
+                }
+                //提前检验重复可以减少外层循环，毕竟pointers元素个数和primes一致，不是长数据不耗时 pointers元素的值是dp元素的指针
+                for (int i1 = 0; i1 < pointers.length; i1++) {
+                    if (dp[pointers[i1]] * primes[i1] == minVal) {
+                        pointers[i1] ++;
+                    }
+                }
+                dp[i] = minVal;
+                i ++;
+            }
+            return dp[i -1];
+        }
     }
 
     public static void main(String[] args) {
-        logger.info("{}", new Solution().nthSuperUglyNumber(1000000, new int[]{2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,
+        logger.info("cost time{}", System.currentTimeMillis());
+        logger.info("{}", new Solution().nthSuperUglyNumber3(1000000, new int[]{2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,
         89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,
         271,277,281,283,293,307,311,313,317,331,337,347,
             349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461
-            ,463,467,479,487,491,499,503,509,521,523,541}));//1
+            ,463,467,479,487,491,499,503,509,521,523,541}));//6262476
 
-//        logger.info("{}", new Solution().nthSuperUglyNumber(100000,
+        logger.info("cost time{}", System.currentTimeMillis());
+
+//        logger.info("{}", new Solution().nthSuperUglyNumber1(100000,
 //            new int[]{7,19,29,37,41,47,53,59,61,79,83,89,101,103,109,127,131,137,139,
 //                157,167,179,181,199,211,229,233,239,241,251}));//1092889481
 
-//        logger.info("{}", new Solution().nthSuperUglyNumber(12, new int[]{2,7,13,19}));//32   [1,2,4,7,8,13,14,16,19,26,28,32]
+        logger.info("cost time{}", System.currentTimeMillis());
+//
+        logger.info("{}", new Solution().nthSuperUglyNumber3(12, new int[]{2,7,13,19}));//32   [1,2,4,7,8,13,14,16,19,26,28,32]
+//        logger.info("cost time{}", System.currentTimeMillis());
 //        logger.info("{}", new Solution().nthSuperUglyNumber(1, new int[]{2,3,5}));//1
     }
 }
